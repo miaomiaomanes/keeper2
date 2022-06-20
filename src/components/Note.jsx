187e744c5c5d5ef65c55367deba454e15afff74b
miaomiao manes <94736807+miaomiaomanes@users.noreply.github.com>
2022-06-20 21:24:46 +0200

    edit edit

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Note(props) {
-  function handleClick() {
+  const [editing, setEditing] = useState(false);
+
+  function doDelete() {
     props.onDelete(props.id);
   }
+
+  function doEdit() {
+    console.log("edit item:", props.id);
+    setEditing(true);
+  }
+
+  if (editing === false) {
+    return (
+      <div className="note">
+        <h1>{props.title}</h1>
+        <p>{props.content}</p>
+        <button onClick={doDelete}>DELETE</button>
+        <button onClick={doEdit}>EDIT</button>
+      </div>
+    );
+  }
+
   return (
     <div className="note">
-      <h1>{props.title}</h1>
-      <p>{props.content}</p>
-      <button onClick={handleClick}>DELETE</button>
+      <input value={props.title} />
+      <textarea value={props.content} />
+      <button onClick={() => {}}>Done</button>
     </div>
   );
 }
